ee90dcf973382f4870f45ac4a101a2e475264e5b
Shane Xu <xusheng0711@gmail.com>
2024-11-05 14:31:59 +0800

    pretty print

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use reqwest::Url;
+use reqwest::{Client, Url};
+use scraper::{Html, Selector};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -28,25 +29,53 @@ struct ApiResponse {
     data: Data,
 }
 
-async fn suggest(q: &str) -> Result<ApiResponse, Box<dyn std::error::Error>> {
+async fn suggest(client: &Client, q: &str) -> Result<ApiResponse, Box<dyn std::error::Error>> {
     let mut url =
         Url::parse("https://dict.youdao.com/suggest?num=5&ver=3.0&doctype=json&cache=false&le=en")?;
     url.query_pairs_mut().append_pair("q", q);
-    let response = reqwest::get(url).await?.json::<ApiResponse>().await?;
+    let response = client.get(url).send().await?.json::<ApiResponse>().await?;
     Ok(response)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = suggest("test").await?;
+    let client = Client::builder().user_agent("curl/8.10.1").build()?;
+    let resp = suggest(&client, "test").await?;
     println!("{resp:#?}");
 
-    let body = reqwest::get("https://dict.youdao.com/result?word=fire&lang=en")
+    let body = client
+        .get("https://dict.youdao.com/result?word=fire&lang=en")
+        .send()
         .await?
         .text()
         .await?;
-
-    println!("{}", body);
-
+    let dom = Html::parse_document(&body);
+    let phone_con_selector = Selector::parse(".phone_con")?;
+    let phone_con_content = dom
+        .select(&phone_con_selector)
+        .into_iter()
+        .next()
+        .unwrap()
+        .html();
+    let phone_con_content = html2text::from_read(phone_con_content.as_bytes(), 200).unwrap();
+    println!("{}", phone_con_content);
+    let simple_dict_selector = Selector::parse(".simple.dict-module")?;
+    let simple_dict_content = dom
+        .select(&simple_dict_selector)
+        .into_iter()
+        .next()
+        .unwrap()
+        .html();
+    let simple_dict_content = html2text::from_read(simple_dict_content.as_bytes(), 200).unwrap();
+    println!("{}", simple_dict_content);
+    let catalogue_sentence_selector = Selector::parse("#catalogue_sentence .dict-book")?;
+    let catalogue_sentence_content = dom
+        .select(&catalogue_sentence_selector)
+        .into_iter()
+        .next()
+        .unwrap()
+        .html();
+    let catalogue_sentence_content = html2text::from_read(catalogue_sentence_content.as_bytes(), 200).unwrap();
+    println!("{}", catalogue_sentence_content);
     Ok(())
 }
