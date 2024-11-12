bfbbc026ce8fa0d170151c56bbb2f712568bbdb0
Shane Xu <xusheng0711@gmail.com>
2024-11-13 00:07:54 +0800

    phone_con

--- src/youdao.rs
+++ src/youdao.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 
 use reqwest::{Client, Url};
-use scraper::{Html, Selector};
+use scraper::{selectable::Selectable, Html, Selector};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -75,24 +75,8 @@ fn parse_word_result_body(word: &str, body: &str) -> Result<WordResult, Box<dyn 
         .map(|h| html2text::from_read(h.as_bytes(), 200).unwrap())
         .unwrap_or_default();
 
-    let phone_con_selector = Selector::parse(".phone_con")?;
-    let phone_con = dom
-        .select(&phone_con_selector)
-        .into_iter()
-        .next()
-        .map(|t| t.html())
-        .map(|h| html2text::from_read(h.as_bytes(), 200).unwrap())
-        .unwrap_or_default();
-
-    let simple_dict_selector = Selector::parse(".simple.dict-module")?;
-    let simple_dict = dom
-        .select(&simple_dict_selector)
-        .into_iter()
-        .next()
-        .map(|t| t.html())
-        .map(|h| html2text::from_read(h.as_bytes(), 200).unwrap())
-        .unwrap_or_default();
-
+    let phone_con = parse_phone_con(&dom).unwrap_or_default();
+    let simple_dict = parse_simple_dict(&dom).unwrap_or_default();
     let catalogue_sentence = parse_catalogue_sentence(&dom).unwrap_or_default();
 
     Ok(WordResult {
@@ -105,13 +89,39 @@ fn parse_word_result_body(word: &str, body: &str) -> Result<WordResult, Box<dyn 
     })
 }
 
+fn parse_phone_con(dom: &Html) -> Option<String> {
+    let per_phone_selector = Selector::parse(".phone_con .per-phone").unwrap();
+    Some(
+        dom.select(&per_phone_selector)
+            .map(|el| format!("- {}", el.text().collect::<Vec<_>>().join(" ")))
+            .collect::<Vec<String>>()
+            .join(" "),
+    )
+}
+
+fn parse_simple_dict(dom: &Html) -> Option<String> {
+    let simple_dict_selector = Selector::parse(".simple.dict-module").unwrap();
+    let simple_dict_el = dom.select(&simple_dict_selector).into_iter().next()?;
+    let word_exp_selector = Selector::parse(".word-exp").unwrap();
+    let mut word_exps = simple_dict_el
+        .select(&word_exp_selector)
+        .map(|el| format!("- {}", el.text().collect::<Vec<_>>().join(" ")))
+        .collect::<Vec<String>>();
+    let word_wfs_less_selector = Selector::parse(".word-wfs-less").unwrap();
+    let word_wfs_less_opt = simple_dict_el
+        .select(&word_wfs_less_selector)
+        .next()
+        .map(|el| format!("- {}", el.text().collect::<Vec<_>>().join(" ")));
+    if let Some(word_wfs_less) = word_wfs_less_opt {
+        word_exps.push(word_wfs_less)
+    }
+    Some(word_exps.join("\n"))
+}
+
 fn parse_catalogue_sentence(dom: &Html) -> Option<String> {
     let catalogue_sentence_selector =
         Selector::parse("#catalogue_sentence .dict-book ul > li").unwrap();
-    let els = dom
-        .select(&catalogue_sentence_selector)
-        .into_iter()
-        .enumerate();
+    let els = dom.select(&catalogue_sentence_selector).enumerate();
     let sen_eng_selector = Selector::parse(".sen-eng").unwrap();
     let sen_ch_selector = Selector::parse(".sen-ch").unwrap();
     let secondary_selector = Selector::parse(".secondary").unwrap();
@@ -119,18 +129,12 @@ fn parse_catalogue_sentence(dom: &Html) -> Option<String> {
         els.flat_map(|(index, el)| {
             let eng = el
                 .select(&sen_eng_selector)
-                .into_iter()
                 .next()
                 .map(|x| x.inner_html())
                 .map(|x| x.replace("<b>", "**").replace("</b>", "**"))?;
-            let cn = el
-                .select(&sen_ch_selector)
-                .into_iter()
-                .next()
-                .map(|x| x.inner_html())?;
+            let cn = el.select(&sen_ch_selector).next().map(|x| x.inner_html())?;
             let dict = el
                 .select(&secondary_selector)
-                .into_iter()
                 .next()
                 .map(|x| x.inner_html())?;
             let idx = index + 1;
@@ -148,10 +152,14 @@ fn parse_catalogue_sentence(dom: &Html) -> Option<String> {
 
 #[cfg(test)]
 #[test]
-fn test_parse_catalogue_sentence() -> Result<(), Box<dyn Error>> {
+fn test_parse() -> Result<(), Box<dyn Error>> {
     use std::fs;
     let body = fs::read_to_string("fire.html").unwrap();
     let dom = Html::parse_document(&body);
+
+    println!("{}", parse_phone_con(&dom).unwrap());
+    println!("{}", parse_simple_dict(&dom).unwrap());
     println!("{}", parse_catalogue_sentence(&dom).unwrap());
+
     Ok(())
 }
